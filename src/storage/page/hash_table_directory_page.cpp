36a984e6a231633da73a7583c5cc709a742a5110
Ubuntu <stan47@Ubuntu18.fnujow5ykr2ufg2etiic5okcfd.bx.internal.cloudapp.net>
2022-02-01 10:52:30 +0000

    lab2

--- src/storage/page/hash_table_directory_page.cpp
+++ src/storage/page/hash_table_directory_page.cpp
@@ -24,31 +24,81 @@ lsn_t HashTableDirectoryPage::GetLSN() const { return lsn_; }
 
 void HashTableDirectoryPage::SetLSN(lsn_t lsn) { lsn_ = lsn; }
 
-uint32_t HashTableDirectoryPage::GetGlobalDepth() { return global_depth_; }
+uint32_t HashTableDirectoryPage::GetGlobalDepth() { 
+  /** TODO **/
+  return global_depth_; 
+}
 
-uint32_t HashTableDirectoryPage::GetGlobalDepthMask() { return 0; }
+uint32_t HashTableDirectoryPage::GetGlobalDepthMask() {
+  /** TODO **/ 
+  uint32_t mask = (0x1 << global_depth_) - 1;
+  return mask; 
+}
 
-void HashTableDirectoryPage::IncrGlobalDepth() {}
+void HashTableDirectoryPage::IncrGlobalDepth() {
+  /** TODO **/
+  global_depth_++;
+}
+
+/** ADD **/
+bool HashTableDirectoryPage::CanIncrGlobalDepth(){ 
+  if((0x1 << (global_depth_ + 1)) <= DIRECTORY_ARRAY_SIZE){
+    return true;
+  }
+  return false;
+};
 
 void HashTableDirectoryPage::DecrGlobalDepth() { global_depth_--; }
 
-page_id_t HashTableDirectoryPage::GetBucketPageId(uint32_t bucket_idx) { return 0; }
+page_id_t HashTableDirectoryPage::GetBucketPageId(uint32_t bucket_idx) { 
+  /** TODO **/
+  return bucket_page_ids_[bucket_idx}]; 
+}
 
-void HashTableDirectoryPage::SetBucketPageId(uint32_t bucket_idx, page_id_t bucket_page_id) {}
+void HashTableDirectoryPage::SetBucketPageId(uint32_t bucket_idx, page_id_t bucket_page_id) {
+  /** TODO **/
+  bucket_page_id_[bucket_idx] = bucket_page_id; 
+}
 
-uint32_t HashTableDirectoryPage::Size() { return 0; }
+uint32_t HashTableDirectoryPage::Size() { 
+  /** TODO **/
+  return 0x1<<global_depth_; ; 
+}
 
-bool HashTableDirectoryPage::CanShrink() { return false; }
+bool HashTableDirectoryPage::CanShrink() { 
+  /** TODO **/
+  for (size_t i = 0; i < Size(); ++i) {
+    if (local_depths_[i] >= global_depth_) {
+      return fasle;
+    }
+  }
+  return true; 
+}
 
-uint32_t HashTableDirectoryPage::GetLocalDepth(uint32_t bucket_idx) { return 0; }
+uint32_t HashTableDirectoryPage::GetLocalDepth(uint32_t bucket_idx) { 
+  /** TODO **/
+  return local_depths_[bucket_idx]; 
+}
 
-void HashTableDirectoryPage::SetLocalDepth(uint32_t bucket_idx, uint8_t local_depth) {}
+void HashTableDirectoryPage::SetLocalDepth(uint32_t bucket_idx, uint8_t local_depth) {
+  /** TODO **/
+  local_depths_[bucket_idx] = local_depth;
+}
 
-void HashTableDirectoryPage::IncrLocalDepth(uint32_t bucket_idx) {}
+void HashTableDirectoryPage::IncrLocalDepth(uint32_t bucket_idx) {
+  /** TODO **/
+  local_depths_[bucket_idx]++;
+}
 
-void HashTableDirectoryPage::DecrLocalDepth(uint32_t bucket_idx) {}
+void HashTableDirectoryPage::DecrLocalDepth(uint32_t bucket_idx) {
+  /** TODO **/
+  local_depths_[bucket_idx]--;
+}
 
-uint32_t HashTableDirectoryPage::GetLocalHighBit(uint32_t bucket_idx) { return 0; }
+uint32_t HashTableDirectoryPage::GetLocalHighBit(uint32_t bucket_idx) {
+  /** TODO **/
+  return bucket_idx & (0x1 << GetLocalDepth(bucket_idx)); 
+}
 
 /**
  * VerifyIntegrity - Use this for debugging but **DO NOT CHANGE**
